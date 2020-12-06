1b2aa2f29ec943f6e3f59190a99b435fa253a249
Jordi Milla Catalán <jb.90@hotmail.es>
2020-12-07 00:33:54 +0100

    Install pods.

---
+++ EmailPeople/EmailPeople/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmailPeople
+//
+//  Created by Jordi Milla on 06/12/2020.
+//  Copyright © 2020 Jordi Milla Catalan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ EmailPeople/Pods/Target Support Files/Pods-EmailPeople/Pods-EmailPeople-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EmailPeople : NSObject
+@end
+@implementation PodsDummy_Pods_EmailPeople
+@end
